4a91f1d0ea3f8836744f3e82fef2cea88f1fea2a
David Varillas <vabedav@gmail.com>
2019-03-09 18:35:38 +0100

    modificacions al app.php

--- app.php
+++ app.php
@@ -13,12 +13,12 @@ require(__DIR__.'/lib/JSONParser.php');
 ####################################################
 
 # Primer instanciar la classe, amb la base URL on hi ha la nostre api
-$base_url   = 'http://xxxxxxxx';
-$token      = 'xxxxxxxxxxxxx';
+$base_url   = 'http://localhost:3000';
+$token      = 'C0UsWlYxXrMx81TKN2Eq';
 $client     = new HTTPClient($base_url, $token);
 # Si necessitem enviar paramatres a l'API, primer crearem un array amb els values
 $params = [
-    'camp1' => 'xxxxxxxxx',
+    'camp1' => '$alumne['nom'],
     'camp2' => 'xxxxxxxxx',
 ];
 
@@ -28,10 +28,10 @@ $params = [
 # Aquesta petició sempre ens retornarà un array amb dos camps:
 ## status =  true / false 
 ## data = retorn de l'API
-$result = $client->query('endpoint');
-$result = $client->query('endpoint', $params, 'POST');
-$result = $client->query('endpoint', $params, 'PUT');
-$result = $client->query('endpoint', [], 'DELETE');
+$result = $client->query('/api/v1/alumnes');
+$result = $client->query('/api/v1/alumne', $params, 'POST');
+$result = $client->query('/api/v1/alumne/:id', $params, 'PUT');
+$result = $client->query('/api/v1/alumne/:id', [], 'DELETE');
 
 
 ####################################################
@@ -39,7 +39,7 @@ $result = $client->query('endpoint', [], 'DELETE');
 ###########  EXEMPLE parser JSON ###################
 ####################################################
 ####################################################
-$result = JSONParser::parseFile(__DIR__.'/route/to/file.json');
+$result = JSONParser::parseFile(__DIR__.'.\alumnes.json');
 # $result contindrà dos camps
 ## status =  true / false 
 ## data = array amb els valors del json o el codi d'error
